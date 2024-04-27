43af36115ec6fe0c1a26925baa1eeb35f360af31
no0000N1 <155792548+no0000N1@users.noreply.github.com>
2024-04-27 16:06:23 +0700

    edit some function

--- telenummanager.c
+++ telenummanager.c
@@ -10,10 +10,6 @@ Tên đơn vị: Công ty ABC
 Số điện thoại: 123456789
 */
 
-const int MAX_SIZE = 1000;
-typedef unsigned long long Number;
-typedef int Position;
-
 // danh sách mẫu tin
 struct NumberInfo
 {
@@ -21,7 +17,7 @@ struct NumberInfo
     char *owner;
     char *address;
     char *nhamang;
-    int number;
+    char *number;
 };
 
 typedef struct NumberInfo *numberInfo;
@@ -34,36 +30,77 @@ struct Node
     struct Node *prev;
 };
 
-typedef struct Node* List;
-typedef struct Node*Position;
-numberList create(int maxsize)
+typedef struct Node *List;
+typedef struct Node *Position;
+
+// create a list of number
+List create()
 {
-    numberList L = malloc(sizeof(struct NumberList));
-    L->maxsize = maxsize;
-    L->count = 0;
-    L->list = malloc(maxsize * sizeof(struct NumberInfo));
+    List L = malloc(sizeof(struct Node));
+    L->next = NULL;
+    L->prev = NULL;
     return L;
 }
 
-//
-void setNumber() {}
+// create one new number element
+struct NumberInfo setNumber(char *number, char *city, char *owner, char *address)
+{
+    struct NumberInfo one;
+
+    one.number = strdup(number);
+    one.city = strdup(city);
+    one.owner = strdup(owner);
+    one.address = strdup(address);
+
+    return one;
+}
 
 // in danh sách thông tin tất cả danh bạ
-void display(numberList L)
+void display(List L)
 {
-    Position p;
-    for (p = 0; p < L->count; p++)
-        printf("City: %s\nUnit: %s\nAddress: %s\nNumber: %lld", L->list[p].city, L->list[p].owner, L->list[p].address, L->list[p].number);
+
+    int count = 1;
+    while (L != NULL)
+    {
+        printf("%d. -Number: %s\n  -City: %s\n  -Unit: %s\n  -Address: %s\n", count++, L->value.number, L->value.city, L->value.owner, L->value.address);
+        L = L->next;
+    }
+    printf("\n");
 }
 
 // chèn vào đầu hoặc cuối danh sách
 void add() {}
 
 // chèn vị trí bất kì
-void insert() {}
+Position insert(List *pL, struct NumberInfo e, Position p)
+{
+    Position newItem = malloc(sizeof(struct Node));
+    newItem->value = e;
+    if (*pL == NULL || p == NULL)
+    {
+        newItem->next = NULL;
+        *pL = newItem;
+    }
+    else
+    {
+        newItem->next = p->next;
+        newItem->prev = p;
+        p->next = newItem;
+        if (newItem->next != NULL)
+        {
+            newItem->next->prev = newItem;
+        }
+    }
+    return newItem;
+}
 
 // tìm kiếm theo các tiêu chí
-Position find() {}
+Position find(List pL, char *city)
+{
+    while (pL != NULL && strcmp(pL->value.city, city) != 0)
+        pL = pL->next;
+    return pL;
+}
 
 Position find1() {}
 
@@ -72,14 +109,23 @@ Position find2() {}
 Position find3() {}
 
 // sắp xếp theo các tiêu chí, @override
-void Arrange() {}
+void Arrange1() {}
 
-void Arrange() {}
+void Arrange2() {}
 
-void Arrange() {}
+void Arrange3() {}
 
 // xóa mẫu tin
-void delete() {}
+void delete(List L, char* city)
+{
+    Position p;
+    p=find(L, city);
+    if (p == NULL)
+        return;
+    p->prev->next = p->next;
+    if (p->next != NULL)
+        p->next->prev = p->prev;
+}
 
 // liệt kê danh bạ từng tỉnh thành
 void list()
@@ -95,9 +141,19 @@ void duplicate() {}
 /*                  */
 int main()
 {
+    List Contacts = create();
+    Contacts = NULL;
+    struct NumberInfo s1 = setNumber("0328981817", "Da Nang", "Danh", "DHBK");
+    struct NumberInfo s2 = setNumber("0328981818", "Ha Noi", "Dat", "DHBK");
+    Position p;
+    p = insert(&Contacts, s1, p);
+    p = insert(&Contacts, s2, p);
+    display(Contacts);
+    char *city = "Da Nang";
+    // delete (Contacts, city);
+    // printf("%s", Contacts->value.number);
+    // display(Contacts);
 
-    numberList L;
-    L = create(MAX_SIZE);
-
+    free(Contacts);
     return 0;
 }
