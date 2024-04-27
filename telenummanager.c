f64888b783338a044c06dbddebb678c5679fd455
Fenris <hoangfdf8212005@gmail.com>
2024-04-27 12:41:19 +0700

    edit ....

--- telenummanager.c
+++ telenummanager.c
@@ -11,86 +11,89 @@ Số điện thoại: 123456789
 */
 
 const int MAX_SIZE = 1000;
-typedef unsigned long long Number;
 typedef int Position;
 
-//danh sách mẫu tin
-struct NumberInfo{
-    char* city;
-    char* owner;
-    char* address;
-    char* nhamang;
-    Number number;
+// danh sách mẫu tin
+struct NumberInfo
+{
+    char *city;
+    char *owner;
+    char *address;
+    char *nhamang;
+    int number;
 };
 
 typedef struct NumberInfo *numberInfo;
 
-//danh bạ
-struct NumberList{
-    int maxsize;
-    int count;
-    numberInfo list;
+// danh bạ
+struct Node
+{
+    struct Node *next;
+    struct NumberInfo value;
+    struct Node *prev;
 };
 
-typedef struct NumberList *numberList;
-
-numberList create(int maxsize){
+typedef struct Node* List;
+typedef struct Node*Position;
+numberList create(int maxsize)
+{
     numberList L = malloc(sizeof(struct NumberList));
     L->maxsize = maxsize;
     L->count = 0;
-    L->list = malloc(maxsize*sizeof(struct NumberInfo));
+    L->list = malloc(maxsize * sizeof(struct NumberInfo));
     return L;
 }
 
 //
-void setNumber(){}
+void setNumber() {}
 
-//in danh sách thông tin tất cả danh bạ
-void display(numberList L){
+// in danh sách thông tin tất cả danh bạ
+void display(numberList L)
+{
     Position p;
-    for (p = 0; p< L->count; p++)
-        printf("City: %s\nUnit: %s\nAddress: %s\nNumber: %lld",L->list[p].city, L->list[p].owner, L->list[p].address, L->list[p].number);
+    for (p = 0; p < L->count; p++)
+        printf("City: %s\nUnit: %s\nAddress: %s\nNumber: %lld", L->list[p].city, L->list[p].owner, L->list[p].address, L->list[p].number);
 }
 
-//chèn vào đầu hoặc cuối danh sách
-void add(){}
+// chèn vào đầu hoặc cuối danh sách
+void add() {}
 
+// chèn vị trí bất kì
+void insert() {}
 
-//chèn vị trí bất kì
-void insert(){}
+// tìm kiếm theo các tiêu chí
+Position find() {}
 
-//tìm kiếm theo các tiêu chí
-Position find(){}
+Position find1() {}
 
-Position find1(){}
+Position find2() {}
 
-Position find2(){}
+Position find3() {}
 
-Position find3(){}
+// sắp xếp theo các tiêu chí, @override
+void Arrange() {}
 
-//sắp xếp theo các tiêu chí, @override
-void Arrange(){}
+void Arrange() {}
 
-void Arrange(){}
+void Arrange() {}
 
-void Arrange(){}
+// xóa mẫu tin
+void delete() {}
 
-//xóa mẫu tin
-void delete(){}
-
-//liệt kê danh bạ từng tỉnh thành
-void list(){
-
+// liệt kê danh bạ từng tỉnh thành
+void list()
+{
 }
 
-//thống kê số lượng thuê bao theo tỉnh thành
-void count(){}
+// thống kê số lượng thuê bao theo tỉnh thành
+void count() {}
 
-//tìm và thông báo nếu có trùng lặp, xóa
-void duplicate(){}
+// tìm và thông báo nếu có trùng lặp, xóa
+void duplicate() {}
 
 /*                  */
-int main(){
+int main()
+{
 
     numberList L;
     L = create(MAX_SIZE);
